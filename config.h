921b09ede73a14890769b6af5fb06e0dce8dfa88
andrecaldeiras <andrefhcaldeira@proton.me>
2023-09-11 18:19:50 +0100

    Color and UI tweaks

--- config.h
+++ config.h
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.93, alphaUnfocused = 0.6;
+float alpha = 1, alphaUnfocused = 0.85;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -124,6 +124,7 @@ static const char *colorname[] = {
 	"#cccccc",
 	"#555555",
 	"black",
+	"#262626",
 };
 
 
@@ -135,7 +136,7 @@ unsigned int defaultfg = 7;
 unsigned int defaultbg = 0;
 unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
-unsigned int bg = 0, bgUnfocused = 16;
+unsigned int bg = 259, bgUnfocused = 259;
 
 /*
  * Default shape of cursor
